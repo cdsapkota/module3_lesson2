24b518bcd91322bb5aef3fa6c9fa9c65c6804306
cdsapkota <cdsapkota@gmail.com>
2024-11-03 23:12:40 -0600

    question 3 change

--- question3.py
+++ question3.py
@@ -1,6 +1,6 @@
 def unpacking(order):
     for index, element in enumerate(order):
-        print(f"\nCustomer {index + 1}: \n   Customer name: {element[0]} \n   Item Ordered: {element[1]} \n   Quantity: {element[2]}")
+        print(f"\nCustomer {index + 1}: \n   Customer name: {element[0]} \n   Item Ordered: {element[1]} \n   Quantity: {element[2]}\n")
 
 orders = [("Alice", "Laptop", 1), ("Bob", "Camera", 2)]
 unpacking(orders)
